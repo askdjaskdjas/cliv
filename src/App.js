4e712a7395d2db69aa4da15a5f952e70dae6dbd6
askdjaskdjas <134807208+askdjaskdjas@users.noreply.github.com>
2023-06-30 20:46:09 +0000

    asdasd

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import * as React from 'react';
 import Home from './components/home'
 import './index.css';
+import './App.css'
 function App() {
   return (
     <div className='text-white'>
